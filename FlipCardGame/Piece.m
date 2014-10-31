11bdd7d484e270dbda0c923cec659f481953b376
Liu Yang <vrace_studios@hotmail.com>
2014-10-31 16:46:01 +0800

    Flip the piece on touch

--- FlipCardGame/Piece.m
+++ FlipCardGame/Piece.m
@@ -34,7 +34,36 @@
     piece.textureCover = cover;
     piece.pieceType = type;
     
+    piece.userInteractionEnabled = YES;
+    
     return piece;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (self.texture != self.textureContent)
+    {
+        [self flipToContent];
+    }
+}
+
+- (void)flipToContent
+{
+    [self flipToTexture:self.textureContent];
+}
+
+- (void)flipToCover
+{
+    [self flipToTexture:self.textureCover];
+}
+
+- (void)flipToTexture:(SKTexture*)texture
+{
+    SKAction *shrink = [SKAction scaleXTo:0 duration:0.15f];
+    SKAction *replace = [SKAction setTexture:texture];
+    SKAction *unshrink = [SKAction scaleTo:1.0f duration:0.15f];
+    
+    [self runAction:[SKAction sequence:@[shrink, replace, unshrink]]];
 }
 
 @end
